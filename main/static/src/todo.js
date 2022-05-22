d1be0e3574d12f064d8d3e12f3c7d5d4aec999b5
Apple1501 <kunure15@gmail.com>
2022-05-22 16:29:19 +0300

    All task

--- main/static/src/todo.js
+++ main/static/src/todo.js
@@ -1,51 +1,13 @@
 window.onload=function()
 {
-    // function fetchTasks(){
-    //     const host = "http://localhost:8000";
-    //     $.ajax({
-    //         url:host+'/api/task/',
-    //         method: 'GET',
-    //         data: {},
-    //         success: function (tasks,status){
-    //             renderTasks(tasks, filter);
-    //             // printCountTasks(counterField,todoItemCheckbox);
-    //         },
-    //         error: function (response, status){
-    //             console.log(response);
-    //             console.log(status);
-    //         }
-    //         });
-    // }
-
-
-    // function filterTasks(tasks,status){
-    //     for(let i=0; i < tasks.length; i++) {
-    //         const todoItem = new TodoItem(tasks[i].title);
-    //             todoBody.append(todoItem.getHtmlElement());
-            
-            
-    //     }
-    // }
-
-    // fetchTasks()
-
-const addButton=document.getElementsByClassName("search_button")[0]
-const inputFields=this.document.getElementsByClassName("search_input")[0]
-const todoBody=document.getElementsByClassName("todo-app_body")[0]
-
-
-addButton.onclick=()=>{
-    const todo=new TodoItem(inputFields.value)
-    todoBody.append(todo.getHtmlElement())
-
-    const host = "http://localhost:8000";
+    function fetchTasks(){
+        const host = "http://localhost:8000";
         $.ajax({
             url:host+'/api/task/',
             method: 'GET',
             data: {},
-            success: function (response,status){
-                console.log(response);
-                console.log(status);
+            success: function (tasks,status){
+                renderTasks(tasks);
                 // printCountTasks(counterField,todoItemCheckbox);
             },
             error: function (response, status){
@@ -53,6 +15,45 @@ addButton.onclick=()=>{
                 console.log(status);
             }
             });
+    }
+
+
+    function renderTasks(tasks,status){
+        for(let i=0; i < tasks.length; i++) {
+            const todoItem = new TodoItem(tasks[i].title);
+                todoBody.append(todoItem.getHtmlElement());
+            
+            
+        }
+    }
+
+    // fetchTasks()
+
+const addButton=document.getElementsByClassName("search_button")[0]
+const inputFields=this.document.getElementsByClassName("search_input")[0]
+const todoBody=document.getElementsByClassName("todo-app_body")[0]
+
+fetchTasks();
+
+addButton.onclick=()=>{
+    // const todo=new TodoItem(inputFields.value)
+    // todoBody.append(todo.getHtmlElement())
+
+    // const host = "http://localhost:8000";
+    //     $.ajax({
+    //         url:host+'/api/task/',
+    //         method: 'GET',
+    //         data: {},
+    //         success: function (response,status){
+    //             console.log(response);
+    //             console.log(status);
+    //             // printCountTasks(counterField,todoItemCheckbox);
+    //         },
+    //         error: function (response, status){
+    //             console.log(response);
+    //             console.log(status);
+    //         }
+    //         });
 
 }
 
