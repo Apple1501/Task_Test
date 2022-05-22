283450e93b91fd850b3f4bbdf492e130bf7e5130
Apple1501 <kunure15@gmail.com>
2022-05-22 17:36:59 +0300

    Delete button

--- main/static/src/todo-item.js
+++ main/static/src/todo-item.js
@@ -1,12 +1,13 @@
 class TodoItem{
-    constructor(title)
+    constructor(title,id)
     {
         this.title=title;
         this.__createHtmlElement()
         this.__host = "http://localhost:8000";
+        this.__id = id;
     }
 
-__createHtmlElement()
+__createHtmlElement(pk)
 {
     const div=document.createElement('div')
     div.className="todo-app_item todo-item"
@@ -22,6 +23,8 @@ __createHtmlElement()
     div.append(titleSpan,deleteSpan)
 
     this.htmlElement=div;
+
+    this.__id = pk;
 
     deleteSpan.onclick=this.remove.bind(this);
 
@@ -57,8 +60,28 @@ getHtmlElement()
     return this.htmlElement;
 }
 
+// remove(){
+//     this.htmlElement.remove();
+// }
+
 remove(){
-    this.htmlElement.remove();
+    this.__sendDeleteRequest();
+}
+
+__sendDeleteRequest(){
+    const title = this.title;
+    $.ajax({
+        url:this.__host+'/api/task/'+this.__id+"/",
+        method: 'DELETE',
+        data: {},
+        success: (tasks,status) => {
+            this.htmlElement.remove();
+        },
+        error: function (response, status){
+            console.log(response);
+            console.log(status);
+        }
+        });
 }
 }
 
--- main/static/src/todo.js
+++ main/static/src/todo.js
@@ -8,6 +8,7 @@ window.onload=function()
             data: {},
             success: function (tasks,status){
                 renderTasks(tasks);
+                console.log(tasks);
                 // printCountTasks(counterField,todoItemCheckbox);
             },
             error: function (response, status){
@@ -20,15 +21,13 @@ window.onload=function()
 
     function renderTasks(tasks,status){
         for(let i=0; i < tasks.length; i++) {
-            const todoItem = new TodoItem(tasks[i].title);
+            const todoItem = new TodoItem(tasks[i].title,tasks[i].id);
                 todoBody.append(todoItem.getHtmlElement());
-            
-            
+                        
         }
     }
 
-    // fetchTasks()
-
+   
 const addButton=document.getElementsByClassName("search_button")[0]
 const inputField=this.document.getElementsByClassName("search_input")[0]
 const todoBody=document.getElementsByClassName("todo-app_body")[0]
@@ -39,8 +38,7 @@ addButton.onclick=()=>{
     const todo  = new TodoItem(inputField.value);
     debugger
     todo.createTodoItem(todoBody);
-    // todoBody.append(todo.getHtmlElement());
-  
+     
 
 }
 
